c4b78a41b469ddf46e3449ce0ff80d6716927784
yangguowei <bujing18@gmail.com>
2018-07-29 01:48:56 +0800

    v0.0.2 css 相关配置 1)gulp-sass sass/scss编译 2)gulp-sourcemaps 源文件映射 3)gulp-autoprefixer 浏览器兼容前缀设置

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 'use strict'
 
 let gulp = require('gulp')
-let clean = require('gulp-clean')
 let connect = require('gulp-connect')
-
-gulp.task('clean', () => {
-  return gulp.src('dist')
-    .pipe(clean())
-})
+let clean = require('gulp-clean')
+let sass = require('gulp-sass')
+let sourcemaps = require('gulp-sourcemaps')
+let autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('connect', () => {
   connect.server({
@@ -18,16 +16,35 @@ gulp.task('connect', () => {
   })
 })
 
+gulp.task('clean', () => {
+  return gulp.src('dist')
+    .pipe(clean())
+})
+
 gulp.task('html', () => {
   return gulp.src('src/**/*.html')
     .pipe(gulp.dest('dist'))
     .pipe(connect.reload())
 })
 
+gulp.task('css', () => {
+  return gulp.src('src/css/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('/maps'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload())
+})
+
 gulp.task('watch', () => {
   gulp.watch(['src/**/*.html'], ['html'])
+  gulp.watch(['src/css/**/*.scss'], ['css'])
 })
 
 gulp.task('default', ['clean'], () => {
-  gulp.start('html', 'connect', 'watch')
+  gulp.start('html', 'css', 'connect', 'watch')
 })
